c9c82b88f3b75d6fa52f8a4e1ce93f10b32ceca6
georgijrogov <georgijrogov@gmail.com>
2021-05-01 18:43:04 +0300

    Добавьте файлы проекта.

---
+++ LINQapplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQapplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
